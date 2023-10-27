fd64ecdcc485b16f8b7f10b7b1126d5a84bf52a6
souheil-moussa <souheil.imoussa@gmail.com>
2023-10-27 13:20:48 +0300

    On branch master Second commit basic maven project

---
+++ app/src/main/java/com/project/App.java
@@ -0,0 +1,20 @@
+package com.project;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    System.out.print("hello world");
+    }
+}
